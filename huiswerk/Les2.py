d263c90009324987f7a932d34aa889cfaa5dc0fc
Bart <Bartdb2001@gmail.com>
2020-12-09 14:55:00 +0100

    Huiswerk 2de week

---
+++ huiswerk/Les2.py
@@ -0,0 +1,39 @@
+class Character:
+    speed = 40
+    alive = True
+    sprite = None
+    points = 0
+
+    def __init__(self):
+        print("Dit is de constructor van Char")
+
+    def Walk(self):
+        print("Char loopt met snelheid", self.speed)
+    
+
+class Inheritance(Character):
+
+    lives = 3
+    item = None
+
+    def __init__(self):
+        super().__init__()
+        self.speed = 30
+
+    def Walk(self):
+        print("Inheritance is snel, namelijk:", self.speed)
+
+    def Jump(self):
+        print("Inheritance kan blijkbaar springen!")
+
+
+characterA = Character()
+InheritanceX = Inheritance()
+
+characterA.Walk
+InheritanceX.Walk
+InheritanceX.Jump
+
+print(characterA.speed)
+print(marioX.speed)
+print(marioX.lives)
